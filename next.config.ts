6c639c5069523d94cb50540cef3784c7aa98db98
Issa <issa.djehon@gmail.com>
2026-06-11 23:06:09 +0200

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+const repositoryName = "solidarity_congo";
+
 const nextConfig: NextConfig = {
+  basePath: isGitHubActions ? `/${repositoryName}` : undefined,
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
   reactStrictMode: true,
   outputFileTracingRoot: process.cwd(),
+  trailingSlash: true,
 };
 
 export default nextConfig;
